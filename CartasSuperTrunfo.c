4116a02b174824b3af3410c31c1a367774c5fd19
carolfp10 <carol.pestili@gmail.com>
2025-01-20 19:00:21 +0000

    inseri float população, double area, pib, int pontos turisticos, char nome.
    criei o print e scanf de todas as informações
    printf para o usuario ler e escrever a informação
    pedida. e no final printf para aparecer as informação
    que foram pedidas e oque o usuario inseriu.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,38 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+
+    float populacao;
+    double area;
+    double pib;
+    int pontosturisticos;
+    char nome[80];
+
+printf("Nome da cidade:\n");
+scanf("%s", &nome);
+
+printf("Insira a população:\n");
+scanf(" %f", &populacao);
+
+printf("Insira a área:\n");
+scanf(" %e", &area);
+
+printf("Insira o PIB:\n");
+scanf("%e", &pib);
+
+printf("Quantos pontos turisticos:\n");
+scanf("%d", &pontosturisticos);
+
+printf("Nome da cidade: %s\n", nome);
+printf("População da cidade: %f\n", populacao);
+printf("Àrea da cidade: %e\n", area);
+printf("PIB da cidade: %e\n", pib);
+printf("Pontos turisticos da cidade: %d\n", pontosturisticos);
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade. 
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
